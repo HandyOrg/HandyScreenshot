cdc74bc2d1435de0dc2752610e3fc3a9263b4bf7
Dingping Zhang <DingpingZhang@outlook.com>
2020-11-15 23:58:18 +0800

    Add the style of the cursor.

--- HandyScreenshot/Converters/PointOrientationToCursorConverter.cs
+++ HandyScreenshot/Converters/PointOrientationToCursorConverter.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Input;
+using HandyScreenshot.Common;
 
 namespace HandyScreenshot.Converters
 {
-    public class PointOrientationToCursorConverter
+    public class PointOrientationToCursorConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is PointOrientation orientation)) return Binding.DoNothing;
+
+            switch (orientation)
+            {
+                case PointOrientation.Left:
+                case PointOrientation.Right:
+                    return Cursors.SizeWE;
+                case PointOrientation.Top:
+                case PointOrientation.Bottom:
+                    return Cursors.SizeNS;
+                case PointOrientation.Left | PointOrientation.Top:
+                case PointOrientation.Right | PointOrientation.Bottom:
+                    return Cursors.SizeNWSE;
+                case PointOrientation.Right | PointOrientation.Top:
+                case PointOrientation.Left | PointOrientation.Bottom:
+                    return Cursors.SizeNESW;
+                default:
+                    return Cursors.SizeAll;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
     }
 }
