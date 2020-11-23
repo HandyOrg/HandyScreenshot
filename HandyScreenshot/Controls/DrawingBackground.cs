80c4cd9800c2d8dd55d8c6511a817af202daa604
Dingping Zhang <DingpingZhang@outlook.com>
2020-11-24 00:02:10 +0800

    [Sync Drawing Api] Save in progress work.

---
+++ HandyScreenshot/Controls/DrawingBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace HandyScreenshot.Controls
+{
+    internal class DrawingBackground : IDrawable<(ImageSource background, double width, double height)>
+    {
+        public bool CanDraw((ImageSource background, double width, double height) drawingData) => true;
+
+        public void Draw(DrawingContext dc, (ImageSource background, double width, double height) drawingData)
+        {
+            var (background, width, height) = drawingData;
+            var group = new DrawingGroup();
+            RenderOptions.SetBitmapScalingMode(group, BitmapScalingMode.NearestNeighbor);
+            var groupDc = group.Open();
+            groupDc.DrawImage(background, new Rect(0, 0, width, height));
+            groupDc.Close();
+            dc.DrawDrawing(group);
+        }
+    }
+}
